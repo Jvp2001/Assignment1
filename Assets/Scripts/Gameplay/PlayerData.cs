ce6f920318c6acee0c19bc8536b2d20de60d2318
Joshua Petersen <s4006219@glos.ac.uk>
2020-10-15 11:08:55 +0100

    Updated

---
+++ Assets/Scripts/Gameplay/PlayerData.cs
@@ -0,0 +1,34 @@
+// © 2020 Joshua Petersen. All rights reserved.
+/// <file>
+/// <copyright> © 2020 Joshua Petersen. All rights reserved. </copyright>
+/// </file>
+
+ using Assignment1;
+using UnityEngine;
+
+namespace Gameplay
+{
+    /// <summary>
+    /// I am using a delegate to update the UI due to it being better than using update. 
+    /// </summary>
+    /// <param name="newValue"> the newValue of <see cref="PlayerData.amountCollected"/>.</param>
+    public delegate void AmountCollectedChanged(short newValue);
+
+    public class PlayerData
+    {
+        private short amountCollected;
+
+        public event AmountCollectedChanged OnAmountCollectedChanged;
+
+        public short AmountCollected
+        {
+            get => amountCollected;
+            set
+            {
+                amountCollected = value;
+
+                OnAmountCollectedChanged?.Invoke(amountCollected);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UserInterface/Game/PlayerHUD.cs
@@ -0,0 +1,44 @@
+// © 2020 Joshua Petersen. All rights reserved.
+/// <file>
+/// <copyright> © 2020 Joshua Petersen. All rights reserved. </copyright>
+/// </file>
+
+ using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assignment1;
+using Assignment1.Gameplay;
+using DG.Tweening;
+using Gameplay;
+using Unity.UIElements.Runtime;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(PanelRenderer))]
+public class PlayerHUD : Singleton<PlayerHUD>
+{
+    
+    private PanelRenderer panelRenderer;
+
+    private Label amountCollected;
+
+    private Label timerLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        panelRenderer = GetComponent<PanelRenderer>();
+        amountCollected = panelRenderer.visualTree.Query<Label>(name: "AmountCollected");
+        timerLabel = panelRenderer.visualTree.Query<Label>(name: "Timer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        panelRenderer.visualTree.MarkDirtyRepaint();
+        
+        timerLabel.text = GameplayManager.Instance.Timer.ToString();
+
+    }
+}
