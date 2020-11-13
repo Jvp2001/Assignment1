6b5b768730849ce8bacf0fba66bbabf7e612ad96
JoshuaPetersen <s4006219@glos.ac.uk>
2020-11-13 09:17:16 +0000

    Almost finished.

---
+++ Assets/Scripts/Gameplay/GemSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Assignment1.Pickups;
+namespace Assignment1.Gameplay
+{
+    public class GemSpawner : MonoBehaviour
+    {
+        [SerializeField] private int totalNumberOfGems;
+
+        private GameObject[] gemContainers;
+
+        private void Start()
+        {
+            SpawnGems();
+        }
+
+        public void SpawnGems()
+        {
+            gemContainers = GameObject.FindGameObjectsWithTag("GemContainer");
+            gemContainers.OrderBy((x) => Random.Range(0, gemContainers.Length)).Take(totalNumberOfGems);
+            GameObject gemPickupPrefab = Resources.Load<GameObject>("Prefabs/GemPickup");
+            foreach (GameObject gemContainer in gemContainers)
+            {
+                GameObject gemPickup = Instantiate(gemPickupPrefab, gemContainer.transform);
+                Gem gem = gemPickup.GetComponent<Gem>();
+                
+                gemPickup.transform.parent = gemContainer.transform;
+                var localPosition = gemPickup.transform.localPosition;
+                gemPickup.transform.localRotation = Quaternion.identity;
+                Vector3 position = gemContainer.GetComponent<Renderer>().bounds.center + Vector3.up;
+                localPosition = position;
+                gemPickup.transform.localPosition = localPosition;
+                Logger.Log($"{gemContainer.name}: Spawned Gem!");
+                gem.IsTrigger = false;
+            }
+        }
+    }
+}
